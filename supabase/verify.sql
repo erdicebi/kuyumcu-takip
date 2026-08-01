e8401cbe76100391fb967ada10fe726568a6de44
erdicebi <erdicebi@gmail.com>
2026-08-01 17:33:13 +0300

    Delete supabase directory

--- supabase/verify.sql
+++
@@ -1,6 +0,0 @@
--- Kurulumdan sonra Supabase SQL Editor'da çalıştırılabilecek salt-okunur kontrol.
-select
-  (select last_number from public.invoice_counters where counter_key = 'invoice') as son_numara,
-  (select count(*) from public.invoices) as fatura_sayisi,
-  (select relrowsecurity from pg_class where oid = 'public.invoices'::regclass) as rls_aktif,
-  (select relforcerowsecurity from pg_class where oid = 'public.invoices'::regclass) as rls_zorunlu;
