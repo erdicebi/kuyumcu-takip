5d836eaff6d8eee4c4129873d4e1b2eae80ce8e1
erdicebi <erdicebi@gmail.com>
2026-08-03 16:53:51 +0300

    Delete public directory

--- public/sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE = "kuyumcu-takip-v1";
-const APP_SHELL = ["/", "/manifest.webmanifest", "/icon.svg"];
-self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL))));
-self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then((cached) => cached || caches.match("/"))));
-});
